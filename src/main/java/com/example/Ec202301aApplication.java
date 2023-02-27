275f1e76d43cae2a11feaba5cabfa81b43e81112
ichiken1018 <ichiken1018@outlook.jp>
2023-02-27 21:23:34 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/Ec202301aApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202301aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202301aApplication.class, args);
+	}
+
+}
