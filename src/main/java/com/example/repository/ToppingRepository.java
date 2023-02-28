8993be0528d33ea7745ed76a7db182f17a406657
k11m2ok <k11m2ok@gmail.com>
2023-02-28 12:26:30 +0900

    各リポジトリの作成

---
+++ src/main/java/com/example/repository/ToppingRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * toppingsテーブルを操作するリポジトリ.
+ * 
+ * @author matsuokatoshiichi
+ *
+ */
+@Repository
+public class ToppingRepository {
+
+}
