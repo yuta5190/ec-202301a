dd03db3dc36452f222b1de3534ad8c7dfe95100a
yuta5190 <yuutatoko0000@gmail.com>
2023-02-28 11:57:30 +0900

    toString getter/setterを作製しました.

--- src/main/java/com/example/domain/Order.java
+++ src/main/java/com/example/domain/Order.java
@@ -11,15 +11,15 @@ import java.util.List;
  *
  */
 public class Order {
+
 	/** id */
 	private Integer id;
 	/** ユーザーid */
 	private Integer userId;
 	/** 注文状況 */
-	/** 0.注文前
-	 * 　1.未入金
-	 * 　2.入金済み
-	 * 　3.発送済み*/
+	/**
+	 * 0.注文前 1.未入金 2.入金済み 3.発送済み
+	 */
 	private Integer status;
 	/** 合計金額 */
 	private Integer totalPrice;
@@ -65,4 +65,149 @@ public class Order {
 		}
 
 	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", userId=" + userId + ", status=" + status + ", totalPrice=" + totalPrice
+				+ ", orderDate=" + orderDate + ", destinationName=" + destinationName + ", destinationEmail="
+				+ destinationEmail + ", destinationZipcode=" + destinationZipcode + ", destinationAddress="
+				+ destinationAddress + ", destinationTel=" + destinationTel + ", deliveryTime=" + deliveryTime
+				+ ", paymentMethod=" + paymentMethod + ",user=" + user + ",orderItemList=" + orderItemList + "]";
+	}
+
+	public Order() {
+	}
+
+	public Order(Integer id, Integer userId, Integer status, Integer totalPrice, Date orderDate, String destinationName,
+			String destinationEmail, String destinationZipcode, String destinationAddress, String destinationTel,
+			Timestamp deliveryTime, Integer paymentMethod, User user, List<OrderItem> orderItemList) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.status = status;
+		this.totalPrice = totalPrice;
+		this.orderDate = orderDate;
+		this.destinationName = destinationName;
+		this.destinationEmail = destinationEmail;
+		this.destinationZipcode = destinationZipcode;
+		this.destinationAddress = destinationAddress;
+		this.destinationTel = destinationTel;
+		this.deliveryTime = deliveryTime;
+		this.paymentMethod = paymentMethod;
+		this.user = user;
+		this.orderItemList = orderItemList;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public String getDestinationName() {
+		return destinationName;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public String getDestinationEmail() {
+		return destinationEmail;
+	}
+
+	public void setDestinationEmail(String destinationEmail) {
+		this.destinationEmail = destinationEmail;
+	}
+
+	public String getDestinationZipcode() {
+		return destinationZipcode;
+	}
+
+	public void setDestinationZipcode(String destinationZipcode) {
+		this.destinationZipcode = destinationZipcode;
+	}
+
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+
+	public String getDestinationTel() {
+		return destinationTel;
+	}
+
+	public void setDestinationTel(String destinationTel) {
+		this.destinationTel = destinationTel;
+	}
+
+	public Timestamp getDeliveryTime() {
+		return deliveryTime;
+	}
+
+	public void setDeliveryTime(Timestamp deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+
+	public Integer getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(Integer paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<OrderItem> getOrderItemList() {
+		return orderItemList;
+	}
+
+	public void setOrderItemList(List<OrderItem> orderItemList) {
+		this.orderItemList = orderItemList;
+	}
+
 }
