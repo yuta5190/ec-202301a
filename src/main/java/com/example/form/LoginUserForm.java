2417168f9fb8d7f6a6808177cd0d9600f51ff3b7
k11m2ok <k11m2ok@gmail.com>
2023-03-01 11:04:23 +0900

    (1-2)ログイン/ログアウトをする　LoginUserFormクラスの作成

---
+++ src/main/java/com/example/form/LoginUserForm.java
@@ -0,0 +1,38 @@
+package com.example.form;
+
+/**
+ * 入力された情報を引き継がせるフォームクラス.
+ * 
+ * @author matsuokatoshiichi
+ *
+ */
+public class LoginUserForm {
+
+	private String email;
+
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginUserForm [email=" + email + ", password=" + password + "]";
+	}
+	
+	
+
+}
