1eca296f54aff14d2ec19a67e54172f9a3b1f8bc
yuta5190 <yuutatoko0000@gmail.com>
2023-03-02 11:06:28 +0900

    コントローラに情報更新のメソッドを作成しました

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,5 +1,23 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.OrderForm;
+import com.example.service.OrderService;
+
+@Controller
+@RequestMapping("/order")
 public class OrderController {
+@Autowired
+private OrderService orderservice;
+
+@PostMapping("/orderinfosend")
+public String orderInfoSend(OrderForm orderform) {
+	orderservice.updateOrder(orderform);
+	return "redirect:order_finish";
+}
 
 }
